56e02969337a415874630b64ab5665f95baa0cda
Irene <okaforirenen@gmail.com>
2020-09-02 03:32:47 +0100

    installed node-sass, added logo image, created header

---
+++ src/components/Header/spec.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./index";
+
+describe( "Haeder component", () => 
+{
+    it( "Should render header without error", () =>
+    {
+        expect( 1 ).toBe( 2 );
+
+    } );
+
+}
+);
